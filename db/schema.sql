76ac26491a1c5b53fd6caf237e41709be090496d
nramos9071 <nramos9071@gmail.com>
2024-11-05 16:49:27 -0600

    updated routes

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,34 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- USE DATABASE
+\c ecommerce_db
+
+-- CREATE TABLES
+CREATE TABLE category (
+  id SERIAL PRIMARY KEY,
+  category_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE product (
+  id SERIAL PRIMARY KEY,
+  product_name VARCHAR(255) NOT NULL,
+  price DECIMAL NOT NULL,
+  stock INTEGER NOT NULL,
+  category_id INTEGER,
+  FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
+);
+
+CREATE TABLE tag (
+  id SERIAL PRIMARY KEY,
+  tag_name VARCHAR(255)
+);
+
+CREATE TABLE product_tag (
+  id SERIAL PRIMARY KEY,
+  product_id INTEGER,
+  tag_id INTEGER,
+  FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE,
+  FOREIGN KEY (tag_id) REFERENCES tag(id) ON DELETE CASCADE
+);
+
