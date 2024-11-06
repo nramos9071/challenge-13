af71b1771061ba4dc8e23eea1cd60ad0c23c1c94
nramos9071 <nramos9071@gmail.com>
2024-11-06 14:59:16 -0600

    updates

--- db/schema.sql
+++ db/schema.sql
@@ -44,20 +44,3 @@ BEGIN
     ALTER TABLE product ADD CONSTRAINT product_category_id_fkey FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE;
 END $$;
 
-SELECT
-    conname AS constraint_name,
-    conrelid::regclass AS table_name,
-    a.attname AS column_name,
-    confrelid::regclass AS foreign_table_name,
-    af.attname AS foreign_column_name,
-    confupdtype AS update_rule,
-    confdeltype AS delete_rule
-FROM
-    pg_constraint AS c
-    JOIN pg_attribute AS a ON a.attnum = ANY(c.conkey) AND a.attrelid = c.conrelid
-    JOIN pg_attribute AS af ON af.attnum = ANY(c.confkey) AND af.attrelid = c.confrelid
-WHERE
-    c.conrelid = 'product'::regclass
-    AND c.confrelid = 'category'::regclass;
-
-    SELECT * FROM product WHERE category_id NOT IN (SELECT id FROM category);
